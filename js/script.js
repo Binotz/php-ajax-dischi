6d84072bf4712374725fce7bace5d0e343e2fa1a
Binotz <binotto.riccardo@gmail.com>
2022-07-26 22:34:43 +0200

    BONUS done

--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
 var app = new Vue({
     el: '#root',
     data:{
-        albums:[]
+        albums:[],
+        filteredAlbums:[],
+        genres:['All'],
+        genreSelected:'All',
+        APILink: 'http://localhost:8888/php-ajax-dischi/api.php'
     },
     methods:{
         getAlbumsviaApi: function(){
-            axios.get('http://localhost:8888/php-ajax-dischi/api.php')
+            axios.get(this.APILink)
             .then((resp)=>{
                 this.albums = resp.data; 
             })
+        },
+        getGenreviaApi: function(){
+            axios.get(this.APILink)
+            .then(resp =>{
+                let albums = resp.data;
+                albums.forEach(album => {
+                    //se il genere non è presente nella lista dei generi lo pusho
+                    if(!this.genres.includes(album.genre)){
+                        this.genres.push(album.genre);
+                    }
+                });
+            })
+        },
+        albumFiltered: function(){
+            if(this.genreSelected == 'All'){
+                //this.filteredAlbums = [...this.albums];
+                return this.albums;
+            }
+            return this.albums.filter(album =>{
+                return album.genre === this.genreSelected;
+            });
         }
     },
     mounted(){
         this.getAlbumsviaApi();
+        this.getGenreviaApi();
     }
 });
